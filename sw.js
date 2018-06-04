5d8a93ca76bc78a60b7effadeb08a3d52ba66399
Derk <irkedmore@gmail.com>
2018-06-04 00:38:51 -0600

    5

--- sw.js
+++ sw.js
@@ -1,89 +1,26 @@
-var staticCacheName = '1';
-
-addEventListener('activate', event => {
-  event.waitUntil( async function() {
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function (cacheName) {
-          return cacheName.startsWith('okay-') &&
-            cacheName != staticCacheName;
-        }).map(function (cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  }());
-});
-
-addEventListener('activate', event => {
-  event.waitUntil(async function() {
-    console.log(event);
-    // Feature-detect
-    if (self.registration.navigationPreload) {
-      // Enable navigation preloads!
-      await self.registration.navigationPreload.enable();
-      console.log('NavigatorPreload Registered');
-    }
-  }());
-});
-
-addEventListener('install', event =>  {
-  event.waitUntil(async function() {
-    caches.open(staticCacheName).then(function (cache) {
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/',
-	'index.html',
-    	'index.js',
-    	'index.css',
-    	'sw-link.js',
-    	'sw.js'
-      ]),
-      cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
-        return new Request(urlToPrefetch, { mode: 'no-cors' });
-      })).then(function() {
-        console.log('All resources have been fetched and cached.');
+        'index.html',
+        'index.js',
+        'index.css',
+        'sw.js',
+        'sw-link.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });  
       });
     })
-    console.log(event);
-  }());
-});
-
-addEventListener('fetch', event => {
-  event.respondWith(async function(){
-    caches.match(event.preloadResponse)
-      .then(function(response) {
-
-        if (response) {
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            var responseToCache = response.clone();
-
-            caches.open(staticCacheName)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-  }());
-});
-
-
-
-addEventListener('message', event => {
-  if (event.data.action == 'skipWaiting') {
-    self.skipWaiting();
-  }
+  );
 });

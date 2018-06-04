a02c063d4101617a3ad8c8ec881fbb68c1e78ea8
Derk <irkedmore@gmail.com>
2018-06-04 00:25:52 -0600

    3

--- sw.js
+++ sw.js
@@ -37,7 +37,10 @@ addEventListener('install', event =>  {
     	'index.css',
     	'sw-link.js',
     	'sw.js'
-      ]).then(function() {
+      ]),
+      cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+        return new Request(urlToPrefetch, { mode: 'no-cors' });
+      })).then(function() {
         console.log('All resources have been fetched and cached.');
       });
     })

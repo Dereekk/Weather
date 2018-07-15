81ab9dcbab5d5928bf91ef27d6a2859bebd7bfd3
Dereekk <irkedmore@gmail.com>
2018-07-15 17:15:10 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,30 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
-  workbox.precaching.precacheAndRoute([]);
+  workbox.precaching.precacheAndRoute([
+  {
+    "url": "index.css",
+    "revision": "8843be3437ef1b0af31c6450914476d2"
+  },
+  {
+    "url": "images/144.png",
+    "revision": "74eaa2a87dd1a30ab965b4b500aaf922"
+  },
+  {
+    "url": "index.html",
+    "revision": "206e1dc4ffce7189e5d17fc1dc5ca9dd"
+  },
+  {
+    "url": "index.js",
+    "revision": "f03f06fd7324d3da7f8e3e99147a5fff"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "0ee02b8c5fa214c61ce692073c693e63"
+  }
+]);
 
 } else {
   console.log(`Boo! Workbox didn't load 😬`);

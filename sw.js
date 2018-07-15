5c6a41d7473671c5eebac88d80ef03893814538b
Derk <irkedmore@gmail.com>
2018-07-15 17:07:01 -0600

    24567

--- sw.js
+++ sw.js
@@ -1,82 +1,41 @@
-var version = "v1::";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-var cacheFiles = [
-	'./',
-	'index.html',
-    'index.css',
-    'index.js',
-    'manifest.json',
-    'sw.js',
-    'sw-link.js'
-];
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
 
+  workbox.precaching.precacheAndRoute([]);
 
-self.addEventListener("install", function(event) {
-  console.log('WORKER: install event in progress.');
-  event.waitUntil(caches.open(version + 'fundamentals').then(function(cache) {
-        return cache.addAll([
-            './',
-            './index.html',
-            './index.js',
-            './index.css',
-            './manifest.json',
-            './sw.js',
-            './sw-link.js',
-            './images/144.png'
-        ]);
-      }).then(function() {
-        console.log('WORKER: install completed');
-      })
-  );
-});
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
-self.addEventListener("fetch", function(event) {
-  console.log('WORKER: fetch event in progress.');
-  if (event.request.method !== 'GET') {
-    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(function(cached) {
-        var networked = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
-        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-        return cached || networked;
-        function fetchedFromNetwork(response) {
-          var cacheCopy = response.clone();
-          console.log('WORKER: fetch response from network.', event.request.url);
-          caches.open(version + 'pages').then(function add(cache) {
-              cache.put(event.request, cacheCopy);
-            }).then(function() {
-              console.log('WORKER: fetch response stored in cache.', event.request.url);
-            });
-          return response;
-        }
-        function unableToResolve () {
-          console.log('WORKER: fetch request failed in both cache and network.');
-          return new Response('<h1>Service Unavailable</h1>', {
-            status: 503,
-            statusText: 'Service Unavailable',
-            headers: new Headers({
-              'Content-Type': 'text/html'
-            })
-          });
-        }
-      })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+    ],
+  }),
+);
 
-self.addEventListener("activate", function(event) {
-  console.log('WORKER: activate event in progress.');
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-        return Promise.all(
-          keys.filter(function (key) {
-              return !key.startsWith(version);
-            }).map(function (key) {
-              return caches.delete(key);
-            })
-        );
-      }).then(function() {
-        console.log('WORKER: activate completed.');
-      })
-  );
-});
+workbox.routing.registerRoute(
+    'https://api.openweathermap.org/data/2.5/*',
+    workbox.strategies.cacheFirst({
+        cacheName: 'stories',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 50,
+            maxAgeSeconds: 5 * 60, // 5 minutes
+          }),
+          new workbox.cacheableResponse.Plugin({
+            statuses: [0, 200],
+          }),
+        ],
+    }),
+);
